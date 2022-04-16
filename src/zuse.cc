eb39c08ec881cf9adcb74ea8fe1a8832cb164b98
setun-90 <dcdn.96@gmail.com>
2022-04-16 17:46:44 +0200

    Update and improve error message

--- src/zuse.cc
+++ src/zuse.cc
@@ -8,56 +8,56 @@
 #include <iostream>
 #include <vector>
 
-
+using namespace std;
 
 int main(int argc, char **argv) {
-	std::ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
 	if (argc <= 1) {
-		std::cerr << "Verwendung: " << argv[0] << " $Konfig-Datei-Name\n";
+		cerr << "Verwendung: " << argv[0] << " $Konfig-Datei-Name\n";
 		return 1;
 	}
 
-	std::fstream cnf(argv[1], std::ios_base::in);
+	fstream cnf(argv[1], ios_base::in);
 	unsigned s;
-	cnf >> s; TRACE("s = " + std::to_string(s));
+	cnf >> s; TRACE("s = " + to_string(s));
 	wahrspeicher hs(s);
 	{
-		std::string l;
-		cnf >> std::ws;
-		while (std::getline(cnf, l)) {
-			std::istringstream il(l);
+		string l;
+		cnf >> ws;
+		while (getline(cnf, l)) {
+			istringstream il(l);
 			char p;
 			unsigned u;
-			il >> p >> std::hex >> u;
+			il >> p >> hex >> u;
 			switch (p) {
 			case 'r': {
-				hs.ute(u, std::make_shared<recheneinheit>(hs));
+				hs.ute(u, make_shared<recheneinheit>(hs));
 				break;
 			}
 			case 'd': {
 				il.clear();
-				std::string n;
+				string n;
 				il >> n;
 				TRACE("n = " + n);
 				auto m(dlopen(n.c_str(), RTLD_LAZY));
 				auto e(dlerror());
 				if (e) {
-					TRACE("Ladung ist gescheitert: " + std::string(e));
+					TRACE("Ladung ist gescheitert: " + string(e));
 					return 1;
 				}
-				std::unique_ptr<durchgangeinheit::gerat> &&(*abb)(std::istringstream const &);
+				unique_ptr<durchgangeinheit::gerat> &&(*abb)(istringstream const &);
 				*(void **)(&abb) = dlsym(m, "abb");
 				e = dlerror();
 				if (e) {
-					TRACE("Anschalt ist gescheitert: " + std::string(e));
+					TRACE("Anschalt ist gescheitert: " + string(e));
 					return 1;
 				}
 
-				hs.ute(u, std::make_shared<durchgangeinheit>(hs, abb(il)));
+				hs.ute(u, make_shared<durchgangeinheit>(hs, abb(il)));
 				break;
 			}
 			default: {
-				std::cerr << "Unbekannte Einheitsart\n";
+				cerr << "Unbekannte Einheitsart: " << p << '\n';
 			}
 			}
 		}
