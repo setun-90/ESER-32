515cc9c0bee7cf2841ec1fb6133bc4ace522dcb8
setun-90 <dcdn.96@gmail.com>
2022-04-22 12:43:15 +0200

    Properly remove unused parameter warnings

--- src/gerat/prufung.cc
+++ src/gerat/prufung.cc
@@ -2,11 +2,11 @@
 
 using namespace std;
 
-prufung abb(istringstream &i) {
+prufung abb(istringstream &) {
 	return prufung();
 }
 
-void prufung::operator()(durchgangeinheit *d, h64 &a) {
+void prufung::operator()(durchgangeinheit *, h64 &) {
 	return;
 }
 
@@ -14,7 +14,7 @@ void prufung::s(durchgangeinheit *d, h32 z, h32 ab) {
 	for (size_t i(0); i < z; i++)
 		d->s(ab + i, this->e());
 }
-void prufung::l(durchgangeinheit *d, h32 z, h32 ab) {
+void prufung::l(durchgangeinheit *, h32 z, h32) {
 	for (size_t i(0); i < z; i++)
 		this->e.discard(1);
 }
