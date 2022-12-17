0b86937511574ed725821d05f40ba3dbeea92d57
setun-90 <dcdn.96@gmail.com>
2022-12-17 16:19:08 +0100

    Change error handling and prepare for cross-platform loading

--- src/verbindung.cc
+++ src/verbindung.cc
@@ -1,25 +1,35 @@
 #include "durchgangeinheit.h"
-#include <trace.h>
 
-#include <iostream>
+#include <string>
+
+#if defined(__linux__) || defined(__unix__) || defined(__APPLE__) || defined(__MACH__)
 #include <dlfcn.h>
+#elif defined(_WIN32) || defined(_WIN64)
+#include <Windows.h>
+#endif
 
 using namespace std;
 
 
 
 unique_ptr<durchgangeinheit::gerat> vb(char const *n, istringstream &i) {
+#if defined(__linux__) || defined(__unix__) || defined(__APPLE__) || defined(__MACH__)
 	auto m(dlopen(n, RTLD_LAZY));
 	char *f;
-	if ((f = dlerror())) {
-		TRACE(string("Ladung ist gescheitert: ").append(f).c_str());
-		return nullptr;
-	}
+	if ((f = dlerror()))
+		throw runtime_error(string("Ladung ist gescheitert: ").append(f).c_str());
 	unique_ptr<durchgangeinheit::gerat> (*abb)(istringstream &)
 		(reinterpret_cast<unique_ptr<durchgangeinheit::gerat> (*)(istringstream &)>(dlsym(m, "abb")));
-	if ((f = dlerror())) {
-		TRACE(string("Anschalt ist gescheitert: ").append(f).c_str());
-		return nullptr;
-	}
-	return unique_ptr<durchgangeinheit::gerat>(abb(i));
+	if ((f = dlerror()))
+		throw runtime_error(string("Anschalt ist gescheitert: ").append(f).c_str());
+#elif defined(_WIN32) || defined(_WIN64)
+	auto m(LoadLibrary(n));
+	if (!m)
+		throw runtime_error("Ladung ist gescheitert");
+	unique_ptr<durchgangeinheit::gerat> (*abb)(istringstream &)
+		(reinterpret_cast<unique_ptr<durchgangeinheit::gerat> (*)(istringstream &)>(GetProcAddress(m, "abb")));
+	if (!abb)
+		throw runtime_error("Anschalt ist gescheitert");
+#endif
+	return abb(i);
 }
