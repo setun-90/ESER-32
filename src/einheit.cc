78845a5cde3a26c542e0d0bd56740dc8418225de
setun-90 <dcdn.96@gmail.com>
2022-12-19 20:36:55 +0100

    Use new literals

--- src/einheit.cc
+++ src/einheit.cc
@@ -3,13 +3,13 @@
 using namespace std;
 
 h16 vzw(h16 n, h8 p) {
-	return n | ~((n & ((h16)1 << p)) - 1);
+	return n | ~((n & (1_16 << p)) - 1);
 }
 h32 vzw(h32 n, h8 p) {
-	return n | ~((n & (1 << p)) - 1);
+	return n | ~((n & (1_32 << p)) - 1);
 }
 h64 vzw(h64 n, h8 p) {
-	return n | ~((n & ((h64)1 << p)) - 1);
+	return n | ~((n & (1_64 << p)) - 1);
 }
 
 einheit::einheit(wahrspeicher &e):
