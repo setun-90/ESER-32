469a91dde3fbca7209f264e6505a30ddaf53d62a
setun-90 <dcdn.96@gmail.com>
2023-01-01 22:39:47 +0100

    Replace hard-coded path to test device plugin

--- prf/durchgangeinheit.cc
+++ prf/durchgangeinheit.cc
@@ -7,8 +7,12 @@
 using namespace std;
 using namespace kunstspeicher;
 
-int main(void) {
+int main(int argc, char **argv) {
 	ios_base::sync_with_stdio(false);
+	if (argc <= 1) {
+		cerr << string("Usage: ").append(argv[0]).append(" $Prufung_Device_Plugin\n");
+		return 1;
+	}
 
 	/* Main memory */
 	wahrspeicher hs(2048 + 5);
@@ -50,7 +54,7 @@ int main(void) {
 	hs.s(az, static_cast<h64>(0xB040000000000000_64 | (static_cast<h64>(0x00000800_32 - az) << 32) | 0x000003FF_32)); az += 8;
 
 	istringstream i;
-	shared_ptr<einheit> e(durchgangeinheit::verbindung("./debug/lib/prufung.so").abb(hs, i));
+	shared_ptr<einheit> e(durchgangeinheit::verbindung(argv[1]).abb(hs, i));
 	e->an();
 	e->ub(static_cast<h64>(0x0080400000800000_64));
 	this_thread::sleep_for(chrono::milliseconds(1000));
